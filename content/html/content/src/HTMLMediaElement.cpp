a6fc1af19c18249f2e7e5f34d39e5d87da673b2f
Rick Eyre <rick.eyre@hotmail.com>
2014-03-13 13:18:06 -0400

    Bug 983207 - Tracks created through a TrackElement should have a default mode of 'disabled' r=cpearce
    I've refactored the way we set modes as well, making it a part
    of the TextTrack ctors.

--- content/html/content/src/HTMLMediaElement.cpp
+++ content/html/content/src/HTMLMediaElement.cpp
@@ -3951,6 +3951,7 @@ HTMLMediaElement::AddTextTrack(TextTrackKind aKind,
 {
   if (mTextTrackManager) {
     return mTextTrackManager->AddTextTrack(aKind, aLabel, aLanguage,
+                                           TextTrackMode::Hidden,
                                            TextTrackSource::AddTextTrack);
   }
   return nullptr;
